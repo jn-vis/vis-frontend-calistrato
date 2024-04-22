2de3d43331b0022dee805ded47cb1f133b6b1da4
DanielCalistrato <dancalist.dev@gmail.com>
2024-04-22 09:45:20 -0300

    feat: implementando react query client no provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
+'use client';
+
 import ProviderComponent from '@/components/layouts/provider-component';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import '../styles/tailwind.css';
 import { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 
-export const metadata: Metadata = {
-    title: {
-        template: '%s | JOBSNOW - Seu buscador de currículo',
-        default: 'JOBSNOW - Seu buscador de currículo',
-    },
-};
+// export const metadata: Metadata = {
+//     title: {
+//         template: '%s | JOBSNOW - Seu buscador de currículo',
+//         default: 'JOBSNOW - Seu buscador de currículo',
+//     },
+// };
+
 const nunito = Nunito({
     weight: ['400', '500', '600', '700', '800'],
     subsets: ['latin'],
@@ -18,11 +23,15 @@ const nunito = Nunito({
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const [queryClient] = React.useState(() => new QueryClient());
+
     return (
-        <html lang="en">
-            <body className={nunito.variable}>
-                <ProviderComponent>{children}</ProviderComponent>
-            </body>
-        </html>
+        <QueryClientProvider client={queryClient}>
+            <html lang="en">
+                <body className={nunito.variable}>
+                    <ProviderComponent>{children}</ProviderComponent>
+                </body>
+            </html>
+        </QueryClientProvider>
     );
 }
